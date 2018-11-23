b053d73ca2d498bf6607ee78c19739b26455016d
ManuJazz <mjesusaz@alumnos.unex.es>
2018-11-23 18:05:31 +0000

    Commit implementation

--- src/main/java/io/swagger/api/impl/ReportApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/ReportApiServiceImpl.java
@@ -23,13 +23,11 @@ import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-11-21T18:46:33.206Z[GMT]")
 
 public class ReportApiServiceImpl extends ReportApiService {
-    
     //list_set
 	List<ReportItem> reports_list = new ArrayList<>();
 	
     @Override
     public Response addReport(ReportItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
     	ReportItem report_item = new ReportItem();
     	report_item.setIdReport(body.getIdReport());
     	report_item.setDate(body.getDate());
@@ -40,27 +38,35 @@ public class ReportApiServiceImpl extends ReportApiService {
     
     @Override
     public Response deleteReport(Long reportId, String apiKey, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        for(int i = 0 ; i < reports_list.size();i++ ){
+            if(reports_list.get(i).getIdReport()==reportId)
+                reports_list.remove(reports_list.get(i));
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Report Deletd!")).build();
     }
     
     @Override
     public Response getAllReports(SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, this.reports_list.toString())).build();
     }
     
     @Override
     public Response getReportById(Long reportId, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+       ReportItem report_item = new ReportItem();
+       for(int i = 0 ; i < reports_list.size();i++ ){
+            if(reports_list.get(i).getIdReport()==reportId)
+               report_item=reports_list.get(i);
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, report_item.toString())).build();
     }
     
     @Override
     public Response updateReport(ReportItem body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+       for(int i = 0 ; i < reports_list.size();i++ ){
+            if(reports_list.get(i).getIdReport()==reportId)
+               reports_list.get(i)=body;
+        }
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Report Updated!")).build();
     }
     
 }
