771adc6ef6f073d34f42176ed9100226ac8452f2
ManuJazz <mjesusaz@alumnos.unex.es>
2018-11-23 10:43:47 +0000

    Api implemented

--- src/main/java/io/swagger/api/impl/ReportApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/ReportApiServiceImpl.java
@@ -2,12 +2,13 @@ package io.swagger.api.impl;
 
 import io.swagger.api.*;
 import io.swagger.model.*;
-
+import java.util.UUID;
 import io.swagger.model.ReportItem;
 
 
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
@@ -23,22 +24,31 @@ import javax.validation.constraints.*;
 
 public class ReportApiServiceImpl extends ReportApiService {
     
+    //list_set
+	List<ReportItem> reports_list = new ArrayList<>();
+	
     @Override
     public Response addReport(ReportItem body, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    	ReportItem report_item = new ReportItem();
+    	report_item.setIdReport(body.getIdReport());
+    	report_item.setDate(body.getDate());
+    	report_item.setDescription(body.getDescription());
+    	reports_list.add(report_item);
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Report added!")).build();
     }
     
     @Override
     public Response deleteReport(Long reportId, String apiKey, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
+        
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
     
     @Override
     public Response getAllReports(SecurityContext securityContext) throws NotFoundException {
         // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, this.reports_list.toString())).build();
     }
     
     @Override
